f0a0913e56302e147e8655d73dba285608b3bb30
Drew Pereli <drewpereli@gmail.com>
2022-08-17 01:00:47 -0400

    Set up basic system of fetching modules

--- src/autocompletion-provider.ts
+++ src/autocompletion-provider.ts
@@ -3,15 +3,16 @@ import * as vscode from 'vscode';
 export default class AutocompletionProvider
   implements vscode.CompletionItemProvider
 {
+  constructor(private modules: string[]) {}
+
   public provideCompletionItems(
     document: vscode.TextDocument,
     position: vscode.Position,
     token: vscode.CancellationToken
   ): Thenable<vscode.CompletionItem[]> {
-    const items: vscode.CompletionItem[] = [
-      { label: 'abcdefg' },
-      { label: '123456789' },
-    ];
+    const items: vscode.CompletionItem[] = this.modules.map((module) => ({
+      label: module,
+    }));
 
     return Promise.resolve(items);
   }
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,38 @@
 import * as vscode from 'vscode';
 import AutocompletionProvider from './autocompletion-provider';
+import { promises as fs } from 'fs';
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+  const modules = await getModules();
+
   context.subscriptions.push(
     vscode.languages.registerCompletionItemProvider(
       { language: 'elixir', scheme: 'file' },
-      new AutocompletionProvider()
+      new AutocompletionProvider(modules)
     )
   );
 }
 
 export function deactivate() {}
+
+async function getModules(): Promise<string[]> {
+  const files = await vscode.workspace.findFiles('**/*.ex');
+
+  const modules = await Promise.all(
+    files.map((f) => getModuleNameFromFile(f.fsPath))
+  );
+
+  const presentModules: string[] = modules.filter(
+    (m): m is string => typeof m === 'string'
+  );
+
+  return presentModules;
+}
+
+async function getModuleNameFromFile(
+  file: string
+): Promise<string | undefined> {
+  const text = String(await fs.readFile(file));
+
+  return text.match(/defmodule\s((\w|\.)+)/)?.[1];
+}
